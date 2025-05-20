60af5f5fdae51ba0c5be4db6555e3b3ca4c4efca
Adam <adam.krystek03@gmail.com>
2025-05-20 14:20:57 +0200

    added comments

--- utils.py
+++ utils.py
@@ -1,14 +1,33 @@
+"""
+This module contains the functions for calculator app.
+"""
+
 def add(a: int, b: int) -> int:
-    return a+b
+    """
+    This function adds two numbers and returns the result.
+    """
+    return a + b
 
-def substract(a:int, b:int) ->int:
-    return a-b
+def subtract(a: int, b: int) -> int:
+    """
+    This function subtracts two numbers and returns the result.
+    """
+    return a - b
 
-def multiply(a:int, b:int) -> int:
-    return a*b
+def multiply(a: int, b: int) -> int:
+    """
+    This function multiplies two numbers and returns the result.
+    """
+    return a * b
 
-def sub(a:float, b:float) -> float:
-    return a/b
+def divide(a: int, b: int) -> float:
+    """
+    This function divides two numbers and returns the result.
+    """
+    return a / b
 
 def square(a:int) -> int:
     return a*a
+"""
+This function raises to the power two the same numbers    
+"""
