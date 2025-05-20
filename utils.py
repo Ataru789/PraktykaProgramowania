69e7d4867f82541cd410667a1512b62c00436297
Adam <adam.krystek03@gmail.com>
2025-05-20 14:43:43 +0200

    5

--- utils.py
+++ utils.py
@@ -25,9 +25,3 @@ def divide(a: int, b: int) -> float:
     This function divides two numbers and returns the result.
     """
     return a / b
-
-def square(a:int) -> int:
-    return a*a
-"""
-This function raises to the power two the same numbers    
-"""
